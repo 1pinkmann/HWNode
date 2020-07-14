49f4ffb5b6c0cfc3ae18a40acbdfd4d2c1be4a0a
Andrei Sharov <sharov@etorg.com>
2020-07-14 11:49:38 +0300

    request install

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
-const apiKey = '*****************';
+const apiKey = '************nod*****';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
