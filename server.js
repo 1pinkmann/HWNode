88723907574dd84b48ee2c6e218b0768c9b7f0da
Andrei Sharov <sharov@etorg.com>
2020-07-14 11:29:00 +0300

    update

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ app.listen(3000, function () {
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+    res.render('index');
+    console.log(req.body.city);
+});
